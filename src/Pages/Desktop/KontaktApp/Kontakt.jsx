b4616cd02248e6d56b7032c0971aeb7196754a84
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-01-30 13:58:47 +0100

    Added profile image

--- src/Pages/Desktop/KontaktApp/Kontakt.jsx
+++ src/Pages/Desktop/KontaktApp/Kontakt.jsx
@@ -1,7 +1,22 @@
 import React from 'react'
+import "./Kontakt.css"
 
 export default function Kontakt() {
   return (
-    <div>Kontakt</div>
+    <div className='kontaktApp'>
+        <div className='kontaktAppSideBar'>
+            <h2>Kontakt</h2>
+            <hr/>
+            <ul>
+                <li>Joel Storr</li>
+            </ul>
+        </div>
+        <div className='kontaktAppMainView'>
+            <div className='kontaktAppHader'>
+                <img src="images/ProfilePic.PNG" />
+            </div>
+
+        </div>
+    </div>
   )
 }
