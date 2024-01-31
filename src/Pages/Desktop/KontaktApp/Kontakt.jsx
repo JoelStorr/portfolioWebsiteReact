dd41943973e2da23301016cbc6b7afbe6ea97aa6
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-01-31 21:53:14 +0100

    Added skills icon sektion

--- src/Pages/Desktop/KontaktApp/Kontakt.jsx
+++ src/Pages/Desktop/KontaktApp/Kontakt.jsx
@@ -34,11 +34,10 @@ export default function Kontakt() {
           </div>
         </div>
         <div className="kontaktAppAboutMe">
-        <hr />
+          <hr />
           <h2>About Me</h2>
           <p>
-            I'm Joel. A IOS & FrontEnd Developer from Germany. <br/>
-
+            I'm Joel. A IOS & FrontEnd Developer from Germany. <br />
             Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
             nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
             erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
@@ -53,10 +52,122 @@ export default function Kontakt() {
             voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
             Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
             dolor sit amet.
-            <br/>
+            <br />
             <a href="#">Lebenslauf</a>
-
           </p>
+
+          <h2>Skills</h2>
+          <div>
+            <h3>Spoken Language</h3>
+            <div className="kontakSkillSection">
+              <div className="programmingLanguage">
+                <img src="/images/icons/comment-regular.svg"></img>
+                <p>German</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/comment-regular.svg"></img>
+                <p>English</p>
+              </div>
+            </div>
+          </div>
+          <div>
+            <h3>IOS Dev</h3>
+            <div className="kontakSkillSection">
+              <div className="programmingLanguage">
+                <img src="/images/icons/swift.svg"></img>
+                <p>Swift</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/compass-drafting-solid.svg"></img>
+                <p>Swift UI</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/cloud-solid.svg"></img>
+                <p>Cloud Kit</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/map-location-dot-solid.svg"></img>
+                <p>Map Kit</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/vr-cardboard-solid.svg"></img>
+                <p>Scene Kit</p>
+              </div>
+            </div>
+            <h3>Web Dev</h3>
+            <div className="kontakSkillSection">
+              <div className="programmingLanguage">
+                <img src="/images/icons/html5.svg"></img>
+                <p>HTML</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/css3.svg"></img>
+                <p>CSS</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/js.svg"></img>
+                <p>Java Script</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/react.svg"></img>
+                <p>React JS</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/vuejs.svg"></img>
+                <p>Vue JS</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/cube-solid.svg"></img>
+                <p>Three JS</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/cube-solid.svg"></img>
+                <p>Babylon JS</p>
+              </div>
+            </div>
+            <h3>Cross Plattform</h3>
+            <div className="kontakSkillSection">
+              <div className="programmingLanguage">
+                <img src="/images/icons/mobile-solid.svg"></img>
+                <p>React Native</p>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/mobile-solid.svg"></img>
+                <p>Flutter / Dart</p>
+              </div>
+            </div>
+            <h3>Expiremented with</h3>
+            <div className="kontakSkillSection">
+              <div className="programmingLanguage">
+                <img src="/images/icons/php.svg"></img>
+                <div>
+                  <p>Php</p>
+                  <p>Php Symfony</p>
+                  <p>Twig</p>
+                </div>
+              </div>
+              <div className="programmingLanguage">
+                <img src="/images/icons/python.svg"></img>
+                <p>Python</p>
+              </div>
+            </div>
+            {/*
+            
+            <div>
+              <h3>Cross Platform</h3>
+              <ul>
+                <li>React Native</li>
+                <li>Flutter / Dart</li>
+              </ul>
+            </div>
+            <div>
+              <h3>Experimented with</h3>
+              <ul>
+                <li>Php</li>
+                <li>Php Symfony / Twig</li>
+                <li>Python</li>
+              </ul> */}
+          </div>
         </div>
       </div>
     </div>
