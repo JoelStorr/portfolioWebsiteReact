427a9bf79d0ae00e6203c3d0e18b8b532634c068
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-01-31 10:09:52 +0100

    Worked on Kontakt Page

--- src/Pages/Desktop/KontaktApp/Kontakt.jsx
+++ src/Pages/Desktop/KontaktApp/Kontakt.jsx
@@ -3,20 +3,28 @@ import "./Kontakt.css"
 
 export default function Kontakt() {
   return (
-    <div className='kontaktApp'>
-        <div className='kontaktAppSideBar'>
-            <h2>Kontakt</h2>
-            <hr/>
-            <ul>
-                <li>Joel Storr</li>
-            </ul>
+    <div className="kontaktApp">
+      <div className="kontaktAppSideBar">
+        <h2>Kontakt</h2>
+        <hr />
+        <ul>
+          <li>Joel Storr</li>
+        </ul>
+      </div>
+      <div className="kontaktAppMainView">
+        <div className="kontaktAppHader">
+          <img src="images/ProfilePic.PNG" />
+          <h1>Joel Storr</h1>
         </div>
-        <div className='kontaktAppMainView'>
-            <div className='kontaktAppHader'>
-                <img src="images/ProfilePic.PNG" />
-            </div>
+        <div className="iconHolder">
+            
 
+          <img src="images/icons/github.svg" className="kontaktIcons" />
+          <img src="images/icons/linkedin.svg" className="kontaktIcons" />
+          <img src="images/icons/envelope-solid.svg" className="kontaktIcons" />
         </div>
+      </div>
     </div>
-  )
+  );
 }
+
