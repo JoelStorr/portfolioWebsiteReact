0bf8a0918c60eb6ba026821e1fa1662d4793d31d
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-01-31 14:51:38 +0100

    Worked on About ME UI

--- src/Pages/Desktop/KontaktApp/Kontakt.jsx
+++ src/Pages/Desktop/KontaktApp/Kontakt.jsx
@@ -1,11 +1,11 @@
-import React from 'react'
-import "./Kontakt.css"
+import React from "react";
+import "./Kontakt.css";
 
 export default function Kontakt() {
   return (
     <div className="kontaktApp">
       <div className="kontaktAppSideBar">
-        <h2>Kontakt</h2>
+        <h2>About ME</h2>
         <hr />
         <ul>
           <li>Joel Storr</li>
@@ -16,15 +16,49 @@ export default function Kontakt() {
           <img src="images/ProfilePic.PNG" />
           <h1>Joel Storr</h1>
         </div>
-        <div className="iconHolder">
-            
 
-          <img src="images/icons/github.svg" className="kontaktIcons" />
-          <img src="images/icons/linkedin.svg" className="kontaktIcons" />
-          <img src="images/icons/envelope-solid.svg" className="kontaktIcons" />
+        <div className="iconRow">
+          <div className="iconHolder">
+            <img src="images/icons/github.svg" className="kontaktIcons" />
+          </div>
+
+          <div className="iconHolder">
+            <img src="images/icons/linkedin.svg" className="kontaktIcons" />
+          </div>
+
+          <div className="iconHolder">
+            <img
+              src="images/icons/envelope-solid.svg"
+              className="kontaktIcons"
+            />
+          </div>
+        </div>
+        <div className="kontaktAppAboutMe">
+        <hr />
+          <h2>About Me</h2>
+          <p>
+            I'm Joel. A IOS & FrontEnd Developer from Germany. <br/>
+
+            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam
+            nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam
+            erat, sed diam voluptua. At vero eos et accusam et justo duo dolores
+            et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
+            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur
+            sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore
+            et dolore magna aliquyam erat, sed diam voluptua. At vero eos et
+            accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren,
+            no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum
+            dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod
+            tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
+            voluptua. At vero eos et accusam et justo duo dolores et ea rebum.
+            Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum
+            dolor sit amet.
+            <br/>
+            <a href="#">Lebenslauf</a>
+
+          </p>
         </div>
       </div>
     </div>
   );
 }
-
