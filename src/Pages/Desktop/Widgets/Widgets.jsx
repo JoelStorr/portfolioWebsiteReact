f3bcb3d048f1b94b93e8975323851a116fd1d5b1
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-02 19:41:14 +0100

    Added Special Day Widget

--- src/Pages/Desktop/Widgets/Widgets.jsx
+++ src/Pages/Desktop/Widgets/Widgets.jsx
@@ -49,70 +49,75 @@ export default function Widgets() {
 
   return (
     <div className="widgets">
-
-    <div className='widgetRow1'>
-      <div className="todoListWidget">
-        <div className="todoListWidgetHeader">
-          <h2>Todays Todos</h2>
-          <img src="/images/icons/calendar-check-solid.svg" />
+      <div className="widgetRow1">
+        <div className="todoListWidget">
+          <div className="todoListWidgetHeader">
+            <h2>Todays Todos</h2>
+            <img src="/images/icons/calendar-check-solid.svg" />
+          </div>
+          <div className="todoListWidgetBody">
+            <ul>
+              <li>
+                <input
+                  type="checkBox"
+                  checked={checkedOne}
+                  value={checkedOne}
+                  onChange={handleChangeOne}
+                />
+                <label
+                  className={checkedOne ? "todoWidgetLabelDone" : ""}
+                  onClick={handleChangeOne}
+                >
+                  Build a Portfolio Website
+                </label>
+              </li>
+              <li>
+                <input
+                  type="checkBox"
+                  checked={checkedTwo}
+                  value={checkedTwo}
+                  onChange={handleChangeTwo}
+                />
+                <label
+                  className={checkedTwo ? "todoWidgetLabelDone" : ""}
+                  onClick={handleChangeTwo}
+                >
+                  Finish of Apps
+                </label>
+              </li>
+              <li>
+                <input
+                  type="checkBox"
+                  checked={checkedThree}
+                  value={checkedThree}
+                  onChange={handleChangeThree}
+                />
+                <label
+                  className={checkedThree ? "todoWidgetLabelDone" : ""}
+                  onClick={handleChangeThree}
+                >
+                  Get a Job
+                </label>
+              </li>
+            </ul>
+            <button className="todoWidgetAddButton">+</button>
+          </div>
         </div>
-        <div className="todoListWidgetBody">
-          <ul>
-            <li>
-              <input
-                type="checkBox"
-                checked={checkedOne}
-                value={checkedOne}
-                onChange={handleChangeOne}
-              />
-              <label
-                className={checkedOne ? "todoWidgetLabelDone" : ""}
-                onClick={handleChangeOne}
-              >
-                Build a Portfolio Website
-              </label>
-            </li>
-            <li>
-              <input
-                type="checkBox"
-                checked={checkedTwo}
-                value={checkedTwo}
-                onChange={handleChangeTwo}
-              />
-              <label
-                className={checkedTwo ? "todoWidgetLabelDone" : ""}
-                onClick={handleChangeTwo}
-              >
-                Finish of Apps
-              </label>
-            </li>
-            <li>
-              <input
-                type="checkBox"
-                checked={checkedThree}
-                value={checkedThree}
-                onChange={handleChangeThree}
-              />
-              <label
-                className={checkedThree ? "todoWidgetLabelDone" : ""}
-                onClick={handleChangeThree}
-              >
-                Get a Job
-              </label>
-            </li>
-          </ul>
-          <button className="todoWidgetAddButton">+</button>
+        <div className="clockWidget" style={gradiantStyle}>
+          <p id="clockWidgetText">{time}</p>
         </div>
       </div>
-      <div className="clockWidget" style={gradiantStyle}>
-        <p id="clockWidgetText">{time}</p>
+      <div className="widgetRow2">
+        <div className="specialDayWidget">
+          <h1>
+            <span>20</span> Days
+          </h1>
+          <h1>
+            <span>15</span> Hours
+          </h1>
+          <h1><span>10</span> Minutes</h1>
+        </div>
       </div>
-
-    </div>
-    <div className='widgetRow2'>
-      <p>Todo</p>
-    </div>
-    
     </div>
   );
 }
