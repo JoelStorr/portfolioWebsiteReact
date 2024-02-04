cd366c9a9a0b5defb7742c2a520ce16001a66f69
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-02-04 16:10:18 +0100

    Clean up

--- src/Pages/Desktop/XCode/XCode.jsx
+++ src/Pages/Desktop/XCode/XCode.jsx
@@ -1,17 +1,15 @@
-import React from 'react'
-import "./XCode.css"
+import React from "react";
+import "./XCode.css";
 
 export default function XCode() {
   return (
-    <div className='xcode'>
-      
-      <div className='xcodeSideView'>Side View Left</div>
-      <div className='xcodeMainView'>
-        <div className='xcodeCodeView'>Code View</div>
-        <div className='xcodePreviewView'>Code Preview</div>
+    <div className="xcode">
+      <div className="xcodeSideView">Side View Left</div>
+      <div className="xcodeMainView">
+        <div className="xcodeCodeView">Code View</div>
+        <div className="xcodePreviewView">Code Preview</div>
       </div>
-      <div className='xcodeSideView'>Side View Right</div>
-    
+      <div className="xcodeSideView">Side View Right</div>
     </div>
-  )
+  );
 }
