09979c4d47e42187bc2eb43ee1932267b8170f82
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-12 21:32:09 +0100

    Added mobiel tab switch

--- src/Pages/Desktop/Browser/BrowserHeader/BrowserHeader.jsx
+++ src/Pages/Desktop/Browser/BrowserHeader/BrowserHeader.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState, useRef} from "react";
 import "./BrowserHeader.css";
 
 export default function BrowserHeader({
@@ -9,68 +9,188 @@ export default function BrowserHeader({
   setProgramm,
   Programm,
 }) {
-  return (
-    <div className="browserHeader">
-      <div className="tabRow">
-        <div
-          className={
-            activeBrowserTab == browserSite.ShapeCSS
-              ? "browserTab activeTab"
-              : "browserTab"
-          }
-          onClick={() => changeActiveTab(browserSite.ShapeCSS)}
-        >
-          <h3>Shape CSS</h3>
-        </div>
-        <div
-          className={
-            activeBrowserTab == browserSite.Portfolio
-              ? "browserTab activeTab"
-              : "browserTab"
-          }
-          onClick={() => changeActiveTab(browserSite.Portfolio)}
-        >
-          <h3>Portfolio</h3>
-        </div>
-        <div
-          className={
-            activeBrowserTab == browserSite.LinkShare
-              ? "browserTab activeTab"
-              : "browserTab"
-          }
-          onClick={() => changeActiveTab(browserSite.LinkShare)}
-        >
-          <h3>LinkShare</h3>
-        </div>
-        <div
-          className={
-            activeBrowserTab == browserSite.Audiophile
-              ? "browserTab activeTab"
-              : "browserTab"
-          }
-          onClick={() => changeActiveTab(browserSite.Audiophile)}
-        >
-          <h3>Audiophile</h3>
-        </div>
-      </div>
-      <div className="searchBar">
-        {width <= 825 ? (
-          <button onClick={() => setProgramm(Programm.None)}>
-            <img src="/images/icons/chevron-left-solid.svg" />
-            <p>Home</p>
-          </button>
-        ) : (
-          <>
-            <p className="backArrowBrowser navigationButton">&#10132;</p>
-            <p className="navigationButton">&#x2794;</p>
-            <p className="reloadArrowBrowser navigationButton">&#x27F3;</p>
-          </>
-        )}
 
-        <div className="searchField">
-          <p>www.{activeBrowserTab.toLowerCase()}.com</p>
-        </div>
-      </div>
-    </div>
-  );
+  let [showTabs, setShowTabs] = useState(false);
+
+
+  function switchTabs(){
+    setShowTabs(!showTabs);
+  }
+
+
+  function isMobile(){
+
+    if(width < 500){
+
+        return (
+          <div className="browserHeader">
+            <div className="tabRow">
+              <div
+                className={
+                  activeBrowserTab == browserSite.ShapeCSS
+                    ? "browserTab activeTab"
+                    : "browserTab"
+                }
+                onClick={() => changeActiveTab(browserSite.ShapeCSS)}
+              >
+                <h3>Shape CSS</h3>
+              </div>
+              <div
+                className={
+                  activeBrowserTab != browserSite.ShapeCSS
+                    ? "browserTab activeTab"
+                    : "browserTab"
+                }
+                onClick={switchTabs}
+              >
+                <h3>More</h3>
+              </div>
+              {showTabs && (
+                <div className="moreTab" onClick={switchTabs}>
+                  <div className="moreTabContainer">
+                    <ul>
+                      <li
+                        className={
+                          activeBrowserTab == browserSite.Portfolio
+                            ? "activeli"
+                            : ""
+                        }
+                        onClick={() => {
+                          changeActiveTab(browserSite.Portfolio);
+                          switchTabs();
+                        }}
+                      >
+                        Portfolio
+                      </li>
+                      <li
+                        className={
+                          activeBrowserTab == browserSite.LinkShare
+                            ? "activeli"
+                            : ""
+                        }
+                        onClick={() => {
+                          changeActiveTab(browserSite.LinkShare);
+                          switchTabs();
+                        }}
+                      >
+                        LinkShare
+                      </li>
+                      <li
+                        className={
+                          activeBrowserTab == browserSite.Audiophile
+                            ? "activeli"
+                            : ""
+                        }
+                        onClick={() => {
+                          changeActiveTab(browserSite.Audiophile);
+                          switchTabs();
+                        }}
+                      >
+                        Audiophile
+                      </li>
+                    </ul>
+                  </div>
+                </div>
+              )}
+            </div>
+            <div className="searchBar">
+              {width <= 825 ? (
+                <button onClick={() => setProgramm(Programm.None)}>
+                  <img src="/images/icons/chevron-left-solid.svg" />
+                  <p>Home</p>
+                </button>
+              ) : (
+                <>
+                  <p className="backArrowBrowser navigationButton">&#10132;</p>
+                  <p className="navigationButton">&#x2794;</p>
+                  <p className="reloadArrowBrowser navigationButton">
+                    &#x27F3;
+                  </p>
+                </>
+              )}
+
+              <div className="searchField">
+                <p>www.{activeBrowserTab.toLowerCase()}.com</p>
+              </div>
+            </div>
+          </div>
+        );
+
+    } else {
+
+        return (
+          <div className="browserHeader">
+            <div className="tabRow">
+              <div
+                className={
+                  activeBrowserTab == browserSite.ShapeCSS
+                    ? "browserTab activeTab"
+                    : "browserTab"
+                }
+                onClick={() => changeActiveTab(browserSite.ShapeCSS)}
+              >
+                <h3>Shape CSS</h3>
+              </div>
+              <div
+                className={
+                  activeBrowserTab == browserSite.Portfolio
+                    ? "browserTab activeTab"
+                    : "browserTab"
+                }
+                onClick={() => changeActiveTab(browserSite.Portfolio)}
+              >
+                <h3>Portfolio</h3>
+              </div>
+              <div
+                className={
+                  activeBrowserTab == browserSite.LinkShare
+                    ? "browserTab activeTab"
+                    : "browserTab"
+                }
+                onClick={() => changeActiveTab(browserSite.LinkShare)}
+              >
+                <h3>LinkShare</h3>
+              </div>
+              <div
+                className={
+                  activeBrowserTab == browserSite.Audiophile
+                    ? "browserTab activeTab"
+                    : "browserTab"
+                }
+                onClick={() => changeActiveTab(browserSite.Audiophile)}
+              >
+                <h3>Audiophile</h3>
+              </div>
+            </div>
+            <div className="searchBar">
+              {width <= 825 ? (
+                <button onClick={() => setProgramm(Programm.None)}>
+                  <img src="/images/icons/chevron-left-solid.svg" />
+                  <p>Home</p>
+                </button>
+              ) : (
+                <>
+                  <p className="backArrowBrowser navigationButton">&#10132;</p>
+                  <p className="navigationButton">&#x2794;</p>
+                  <p className="reloadArrowBrowser navigationButton">
+                    &#x27F3;
+                  </p>
+                </>
+              )}
+
+              <div className="searchField">
+                <p>www.{activeBrowserTab.toLowerCase()}.com</p>
+              </div>
+            </div>
+          </div>
+        );
+
+    }
+
+    
+  }
+  
+  
+  return isMobile();
+
 }
