cfc19903e7a6101c09e33eb0c17833186be7daf3
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-14 17:14:04 +0100

    Added project text

--- src/Pages/Desktop/Browser/Websites/ShapeCSS.jsx
+++ src/Pages/Desktop/Browser/Websites/ShapeCSS.jsx
@@ -37,166 +37,39 @@ export default function ShapeCSS() {
         </div>
       </div>
       <div className="websiteContentAbout">
-        <h3>Core Project Idea:</h3>
         <p>
-          The main Project Idea is a set of tools for visually creating CSS
-          effects in an easy-to-use way. The app can then return a String back
-          to the user that contains the Apropiate code snipped. As a Bonus, I
-          build a fun little Physik effect for a catchier Presentation into the
-          Landing / front page of the App.
-        </p>
-        <p>
-          Both of the Tools contain the ability to not only represent the
-          current Properties on the screen when they are changed but also to
-          Interact with the Clipboard API. The Landing Page for the Project
-          contains a Libary called Matter JS that allows you to draw shapes into
-          an HTML Canvas and to calculate their Physical behavior on the canvas.
-          (See: https://joelstorr.github.io/shape-css-gp/)
-          <br />
-          In Termes of Mobile Responsiveness, the Page will detect Devices
-          outside the Desktop realm and will tell prompt the user to use a
-          Desktop to access the Application. On the Screen, I provide a Link to
-          the Page, that can be copied over to the CLipBoard to be sent to your
-          computer.
-        </p>
-        <h3>Entry Point to the Project:</h3>
-        <ul>
-          <li>assets</li>
-          <li>components</li>
-          <li>
-            <ul>
-              <li>Base</li>
-              <li>Directives</li>
-              <li>HomeComp</li>
-              <li>toosl</li>
-            </ul>
-          </li>
-          <li>router</li>
-          <li>store</li>
-          <li>views</li>
-          <li>App.vue</li>
-          <li>main.js</li>
-        </ul>
-        <h4>main.js</h4>
-        <p>
-          The main.js file builds the entry point to the Application imports the
-          Vue App, router, and store as well as the Copy Directive, which
-          handles the Interaction with the ClipBoard API. On top of that, it
-          Inits the store and router as well as exposes the Copy directive to
-          the App, before it gets mounted by Vue.
-        </p>
-        <h4>app.vue</h4>
-        <p>
-          The app.vue file builds the main render field for the Application and
-          holds the information to render the view as well as the Logic for
-          deciding if a Desktop or Mobile view of the program should be
-          displayed. Since a Vue file is used it can also hold the Main Styles
-          for the App.
-        </p>
-
-        <h4>assets:</h4>
-        <p>
-          The assets folder is in this case mostly empty and unused but there to
-          be used if the Project is extended in the future and to have a full
-          Vue.js structure.
-        </p>
-
-        <h4>views & components:</h4>
-        <p>
-          The views folder holds the individual views of the Application. The
-          components folder holds the Individual parts of the Application that
-          are rather code-intensive or reused in Multiple Instances. I choose to
-          first explain the View and then the corresponding components to make
-          the Documentation not too scattered.
-        </p>
-
-        <h4>HomeView.vue</h4>
-        <p>
-          The Home view is tasked to handle the rendering of the Initial
-          Matter.js component (src/components/HomeComp/matterJS.vue).
-        </p>
-
-        <h4>matterJs.vue</h4>
-        <p>
-          The matterJs.vue file holds the Main Logic for the Matter.js Physiks
-          engine and handles the drawing of elements onto the Canvas as well as
-          the Implementation of the Physics based on the Matter.js Package.
-          <br />
-          Resources for this part of the Code: <br />
-          Matter.js Docs <br />
-          The Coding Train: Matter.js Physics on YT <br />
-          MenueView.vue <br />
-          This file handles the Display of the menu and Dynamically Loads the
-          right amount of Tool Cards into the View
-        </p>
-
-        <h4>BaseCardComponent.vue</h4>
-
-        <p>
-          This file handles the Display of the Tool Cards and loads the Relevant
-          Icons that get passed into the file via the MenueView.vue as well as
-          pointing the Button to the Correct Application Route.
-        </p>
-
-        <h4>ToolView.vue</h4>
-        <p>
-          Displays the Tool itself and can load the needed tool Component based
-          on the Selected Route (router view). It also continues the Logic to
-          point back to the Main application menu. <br />
-          It can load any of the Tools that are inside the
-          (src/components/tools/ ... .vue).
-        </p>
-
-        <h4>dropShadow.component.vue</h4>
-        <p>
-          This file contains the Logic to display the Drop shadow Tool and uses
-          the Copy component to hook into the ClipBoard API of the Browser.
-        </p>
-
-        <h4>threedtransform.component.vue</h4>
-
-        <p>
-          Holds the Logic for the 3D Transform Tool and Loads in the Copy
-          Component to hook into the ClipBoard API of the Browser.
-        </p>
-
-        <h4>BaseCopy.component.vue</h4>
-        <p>
-          This component uses Copy directive which allows you to copy any peace
-          of String and pass it to be used through the ClipBoard API. Its task
-          is to show the Copy Overlay over the Code Block Directives/copy.js
-          Creates a selected aria of the screen that can be used via the
-          Clipboard API.
-        </p>
-
-        <h4>MobileHome.component.vue</h4>
-        <p>
-          This file gets rendered when the user visits the Page from a non
-          Desktop device. The Device Checking is done through the HomeView.vue
-          file.
-        </p>
-
-        <h4>Router (index.js)</h4>
-        <p>
-          The router manages the Routing of the SPA application and allows for a
-          more Software like feel.
-        </p>
-
-        <h4>Store (index.js)</h4>
-        <p>
-          The store file would allow the user to share properties like color or
-          other info throughout the App. The Structure for that is there but it
-          is not implemented because there isn't a use case jet. Intend to add
-          that beyond the Scope of the final Project.
-        </p>
-
-        <h4>Design</h4>
-        <p>
-          For the Project, I choose to have a High Contrast minimalistic design,
-          which would not only allow the user to see the Shape and Shadow
-          properly inside the Tool section but also make the Physics effect
-          stand out more on the Landing Page.
-        </p>
+          ShapeCSS ist als Abschluss Projekt für mein CS50 Zertifikat
+          entstanden. Mein Ziel war, es ein SPA zu entwickeln welches dem Nutzer
+          unterstützt einige CSS Effekte visuell über die UI zu erzeugen.
+          Zusätzlich reflektiert die UI einen Code Block den der Nutzer per
+          Click kopieren und in sein eigenes Projekt einfügen kann. 
+          </p>
+          <h3>
+            Benutze Technologien: 
+          </h3>
+          <p>
+            Für dieses Projekt habe ich mich für eine VueJS Frontend
+            entscheiden, welches gebrauch vom Vue Router macht, um einen
+            fließenden Übergang zwischen den Seiten zu schaffen, ohne ein neu
+            Laden der Seite zu erzwingen. Für eine visuell spannenderen Projekt
+            Einführung habe ich außerdem auf eine 2D Physik Engine gesetzt, welche
+            die Interaktiven formen auf der Startseite generiert und verwaltet.
+          </p>
+          <h3>Gelerntes</h3>
+          <p>
+            Im Zuge dieses Projekt habe ich mein wissen, über Frontend Technologien,
+            wie VueJS vertieft und habe, mich mit der Integration von Framework
+            fremden NPM packten in ein bestehendes Vue Projekt beschäftigt.
+          </p>
+          <h3>Responsive Design</h3>
+          <p>
+           Da die Tools einen eher geringen Nutzen auf Mobilen entgräten besitzen, habe
+          ich mich gegen eine mobile Optimierung entscheiden, stattdessen zeigt
+          die App auf einem Smartphone einen Infotext, mit der Möglichkeit den
+          Link zur Seite zu Kopieren und für später zu speichern.
+          </p>
+          
+      
       </div>
     </div>
   );
