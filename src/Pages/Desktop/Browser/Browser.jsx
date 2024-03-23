ef597e727ef0f7b04169b51d7c1aa2d9e6c173c3
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-23 21:48:29 +0100

    Clean up

--- src/Pages/Desktop/Browser/Browser.jsx
+++ src/Pages/Desktop/Browser/Browser.jsx
@@ -1,50 +1,45 @@
-import React, { useState } from 'react'
-import "./Browser.css"
-import BrowserHeader from './BrowserHeader/BrowserHeader'
-import ShapeCSS from './Websites/ShapeCSS'
-import Portfolio from './Websites/Portfolio'
-import Audiophile from './Websites/Audiophile'
-import LinkShare from './Websites/LinkShare'
+import React, { useState } from "react";
+import "./Browser.css";
+import BrowserHeader from "./BrowserHeader/BrowserHeader";
+import ShapeCSS from "./Websites/ShapeCSS";
+import Portfolio from "./Websites/Portfolio";
+import Audiophile from "./Websites/Audiophile";
+import LinkShare from "./Websites/LinkShare";
 
-export default function Browser({width, setProgramm, Programm}) {
+export default function Browser({ width, setProgramm, Programm }) {
+  const BrowserSite = {
+    ShapeCSS: "ShapeCss",
+    Portfolio: "Portfolio",
+    Audiophile: "Audiophile",
+    LinkShare: "LinkShare",
+  };
 
+  const [activeTab, setActiveTab] = useState(BrowserSite.ShapeCSS);
 
-    const BrowserSite = {
-        ShapeCSS: "ShapeCss",
-        Portfolio: "Portfolio",
-        Audiophile: "Audiophile",
-        LinkShare: "LinkShare"
+  function displayWebPage() {
+    switch (activeTab) {
+      case BrowserSite.ShapeCSS:
+        return <ShapeCSS />;
+      case BrowserSite.Portfolio:
+        return <Portfolio />;
+      case BrowserSite.Audiophile:
+        return <Audiophile />;
+      case BrowserSite.LinkShare:
+        return <LinkShare />;
     }
-
-    const [activeTab, setActiveTab] = useState(BrowserSite.ShapeCSS)
-
-    function displayWebPage(){
-        switch(activeTab){
-            case BrowserSite.ShapeCSS:
-                return <ShapeCSS />
-            case BrowserSite.Portfolio:
-                return <Portfolio />
-            case BrowserSite.Audiophile:
-                return <Audiophile />
-            case BrowserSite.LinkShare:
-                return <LinkShare />
-        }
-    }
-
+  }
 
   return (
-    <div className='browser'>
-       <BrowserHeader 
-            activeBrowserTab={activeTab} 
-            changeActiveTab={setActiveTab} 
-            browserSite={BrowserSite} 
-            width={width}
-            setProgramm={setProgramm}
-            Programm={Programm}
-        />
-        <div className='browserContent'>
-            {displayWebPage()}
-        </div>
+    <div className="browser">
+      <BrowserHeader
+        activeBrowserTab={activeTab}
+        changeActiveTab={setActiveTab}
+        browserSite={BrowserSite}
+        width={width}
+        setProgramm={setProgramm}
+        Programm={Programm}
+      />
+      <div className="browserContent">{displayWebPage()}</div>
     </div>
-  )
+  );
 }
