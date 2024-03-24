49cefb40c6e251e7d9cc509e83c2a96047687b44
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-24 14:39:26 +0100

    Added Projeckt Text

--- src/Pages/Desktop/AppStore/AppDetail/GitHubFollowers.jsx
+++ src/Pages/Desktop/AppStore/AppDetail/GitHubFollowers.jsx
@@ -51,15 +51,15 @@ export default function GitHubFollowers() {
         {/* TODO: Dispaly Descriptions */}
         <h3>About the Project</h3>
         <p>
-          GitHub Follower Tracker war eines der ersten Tutorialprojekte in die
-          IOS Entwicklung. Die App ist Teil des GitHub Followers Kurs von Sean
+          GitHub Follower Tracker war eines der ersten Tutorialprojekte in der
+          IOS Entwicklung. Die App ist Teil des GitHub Followers Kurses von Sean
           Allen. Dieser Kurs hat mir als Einführung in die Swift Programmierung
-          gedient und mir die Entwicklungsumgebung näher gebracht. Als Teile des
-          Kurses habe ich mich mit Swift UI auseinandergesetzt und mich mit dem
-          laden von API Daten in einer IOS App vertraut gemacht. Ebenfalls habe
-          ich das Navigieren über mehrere Seiten erlernt als auch das dynamische
-          Laden von Bildern aus dem Web. Die App kommt ebenfalls mit einem
-          System zum automatischen Nachladen von Nutzern, wenn man am Ende der
+          gedient und die Entwicklungsumgebung näher gebracht. Als Teil des
+          Kurses habe ich mich mit UI Kit auseinandergesetzt und mit dem Laden
+          von API Daten in einer IOS App vertraut gemacht. Ebenfalls habe ich
+          das Navigieren über mehrere Seiten erlernt als auch das dynamische
+          Laden von Bildern aus dem Web. Die App verwendet ein System zum
+          automatischen Nachladen von Nutzerprofilen, wenn man am Ende der
           geladenen Follower angekommen ist.
         </p>
       </div>
