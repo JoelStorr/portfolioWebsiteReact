9b219d069bb699fbda6098aa3fba5d9d908647dc
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-24 11:48:05 +0100

    Added DDG Images

--- src/Pages/Desktop/AppStore/AppDetail/DubDubGrub.jsx
+++ src/Pages/Desktop/AppStore/AppDetail/DubDubGrub.jsx
@@ -11,24 +11,16 @@ export default function DubDubGrub() {
 
   const slides = [
     {
-      url: "https://images.unsplash.com/photo-1706007171907-49841a64792a?q=80&w=2069&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: `images/DubDubGrub/DStart.jpg`,
       title: "SunFlower",
     },
     {
-      url: "https://images.unsplash.com/photo-1706088604350-f3c46951ee02?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: `images/DubDubGrub/DPlaces.jpg`,
       title: "Mountanse",
     },
     {
-      url: "https://images.unsplash.com/photo-1706273427992-8a1b37142cb8?q=80&w=2070&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+      url: `images/DubDubGrub/DProfile.jpg`,
       title: "Lamps",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1706263085333-653485333e47?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "Chip",
-    },
-    {
-      url: "https://images.unsplash.com/photo-1706211306770-89f0a903eda7?q=80&w=1932&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
-      title: "Space",
     },
   ];
 
