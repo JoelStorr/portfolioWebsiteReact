855586f088af8c97c49a9595f4a6c4e02bda31e7
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-03-28 19:00:26 +0100

    Added three js text

--- src/Pages/Desktop/Browser/Websites/ThreeJSBall.jsx
+++ src/Pages/Desktop/Browser/Websites/ThreeJSBall.jsx
@@ -52,52 +52,18 @@ export default function ThreeJSBall() {
       <div className="websiteContentAbout">
         <h3>Über das Projeckt</h3>
         <p>
-          Fullstack Projekt Linkshare ist eines der Frontend Mentor Guru
-          Projekte in meinem Portfolio. Hierbei stellt Frontend Mentor die
-          Designdateien und eine Projektübersicht zur Verfügung, so dass dem
-          Challenge Teilnehmer selber frei gestellt wird, wie er zu dem
-          gewollten Ergebnis kommt. Das Projekt selbst teilt sich in zwei Teile
-          auf. Das Frontend besteht aus einem Server seitig gerenderten Nuxt
-          Projekt. Dieses bezieht seine Daten durch ein Python FastAPI basiertes
-          Backend.
-          <br />
-          <br />
-          Als Kern des Frontends dient ein Pinia Store, der sowohl die
-          Authentifizierung des Nutzers handhabt, als auch die Link- und
-          Profildaten zur Verfügung stellt, die von der API stammen. Gemanagt
-          wird die Überprüfung des Login Status über die in Nuxt integrierte
-          Middleware. Hierbei wird auf den geschützen Seiten eine
-          Authentifizierung, via API Abfrage, mit dazugehörigen JSON Web Token
-          durchgeführt.
-        </p>
-        <p>
-          Der Editor setzt neben einer Live Preview der Nutzerinformation auch
-          auf ein Drag and Drop System zum Neuanordnen der Nutzerlinks. Hierfür
-          verwendet die App das Vue.Draggable Paket. Dabei wird die Position mit
-          an die API übertragen und beim nächsten Login auch wieder reflektiert.
-          Eine Überprüfung der Nutzereingaben wird nutzerseitig ausgeführt bevor
-          die Daten an den Server übertragen werden.
-        </p>
-        <p>
-          Die App ist in der Lage das vom Nutzer hochgeladene Foto an die API zu
-          übermitteln, und den vom Server kommenden Base64 String wieder in ein
-          Bild zu konvertieren, das dem Nutzer im Frontend angezeigt werden
-          kann.
-        </p>
-        <p>
-          Die App ist sowohl für den Desktop, als auch für Tablet und Smartphone
-          ausgelegt und passt die UI für das beste Nutzererlebnis an. So wird
-          die Echtzeit Preview auf Smartphones entfernt, um mehr Raum für den
-          Editor zu lassen.
-        </p>
-        <p>
-          Das FastAPI Backend ist darauf ausgelegt einen JSON Web Token nach der
-          Anmeldung zu generieren und auf den geschützten API Endpunkten zu
-          verifizieren. In diesem Projekt habe ich auf eine einfache SQLite
-          Datenbank gesetzt. Hierbei sind die Pfade zum Editieren der
-          Informationen via Token Verifizierung gesichert. Das Backend speichert
-          die Bilder als Base64 in einem eigenen Ordner auf dem Server und gibt
-          diese auch als Base64 String wieder an das Frontend zurück.
+          Das 3D Browser Spiel ist als Teil des ThreeJS Journy Kurses von Bruno
+          Simon entstanden. Hierbei setzt das Projekt auf das React Three Fiber
+          Paket (R3F), das die Integration von ThreeJS in React Projekte
+          vereinfacht. Das Spiel wird durch die Rapier Physik Engine betrieben,
+          um die Kollisionen mit den Hindernissen zu betreiben. 
+          <br/> <br/>
+          Das Ziel dieses
+          Projektes wer es, mein Wissen in R3F zu vertiefen und mich mit der
+          neuen Syntax vertraut zu machen. Gerendert wird das Projekt innerhalb
+          eines HTML Canvas elements, wobei ThreeJS auf dem Browser nativen
+          WebGL aufbaut, um das 3D Erlebnis in jedem modernen Browser zur
+          Verfügung zu stellen.
         </p>
       </div>
     </div>
