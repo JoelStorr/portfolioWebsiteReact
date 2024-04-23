9280b3dbf986979279584a5aebb2194451396a1e
Joel Storr <50362041+JoelStorr@users.noreply.github.com>
2024-04-23 10:30:09 +0200

    Added projeckt text

--- src/Pages/Desktop/Browser/Websites/3dTShirtDesigner.jsx
+++ src/Pages/Desktop/Browser/Websites/3dTShirtDesigner.jsx
@@ -56,18 +56,16 @@ export default function ThreeDTShirtDesigner() {
       <div className="websiteContentAbout">
         <h3>Über das Projeckt</h3>
         <p>
-          Das 3D Browser Spiel ist als Teil des ThreeJS Journey Kurses von Bruno
-          Simon entstanden. Hierbei setzt das Projekt auf das React Three Fiber
-          Paket (R3F), das die Integration von ThreeJS in React Projekte
-          vereinfacht. Hierbei lassen sich die 3D Elemente direkt in der JSX
-          Syntax definieren. Das Spiel wird durch die Rapier Physics Engine
-          betrieben, um die Kollision mit den Hindernissen zu berechnen.
-          <br /> <br />
-          Das Ziel dieses Projektes war es, mein Wissen in R3F zu vertiefen und
-          mich mit der neuen Syntax vertraut zu machen. Gerendert wird das
-          Projekt innerhalb eines HTML Canvas Elements. Hierbei baut ThreeJS auf
-          dem Browser nativen WebGL auf, um 3D Erlebnisse in jedem modernen
-          Browser zur Verfügung zu stellen.
+          Das Shirt Designer Projekt ist als Teil meines Interesses an 3D im Web
+          entstanden und basiert auf einem JavaScript Mastery Tutorial. In
+          diesem habe ich mich mit der React Animations Bibliothek Framer Motion
+          beschäftigt, um der UI eine interaktivere Form zu geben. Hierbei setzt
+          das Projekt auf das React Three Fiber Paket (R3F) um das 3D nahtlos in
+          das React Ökosystem einzubinden. 
+          <br/> <br/>
+          Besonders interessant war hierbei das
+          Verändern von 3D Objekten auf Basis der Nutzereingabe und das
+          Platzieren von Bildern auf dem 3D Objekt.
         </p>
       </div>
     </div>
